3c8c5a67ce76bce7ab483de306aa285bcc3a5c58
Cesar Duque Gomez <cduque1@yuxiglobal.com>
2017-09-22 18:37:51 -0500

    Adding ng-messages to control error

--- Angular/publish-events/content/js/components/UserManager.js
+++ Angular/publish-events/content/js/components/UserManager.js
@@ -1,6 +1,6 @@
 var userManager ={
     bindings:{},
-    controller: function($rootScope, $scope){
+    controller: function($rootScope, $scope, $timeout){
         var ctrl = this
         ctrl.user = {
             name:'',
@@ -16,17 +16,35 @@ var userManager ={
         $scope.$on('logout', function(event, data){
             console.log(data);
         });
+        ctrl.$onInit = function(){
+            $timeout(function(){
+                console.log($scope.form);
+            }, 1000);
+            
+        }
 
     },
     template:`
-        <form ng-submit="$ctrl.loginUser();">
+        <form name="form" ng-submit="$ctrl.loginUser();">
             <div>
-                Name: <input type="text" ng-model="$ctrl.user.name">
+                <label for="inputName">Name:</label>
+                <input name="inputName" type="text" ng-model="$ctrl.user.name" required minlength="3">
             </div>
+            <div ng-if="form.inputName.$touched" ng-messages="form.inputName.$error" ng-messages-multiple>
+                <label ng-message="required">
+                    This field is required!!
+                </label>
+                <label ng-message="minlength">
+                 This field must have at least 3 characters!!!
+            </label>
+            
+            </div>
+
             <div>
-                Email: <input type="text" ng-model="$ctrl.user.email">
+                <label for="inputEmail" >Email:</label>
+                <input name="inputEmail" type="text" ng-model="$ctrl.user.email" required>
             </div>
-            <button type="submit">
+            <button type="submit" ng-disabled="form.$invalid">
                 Log in
             </button>
         </form>    
